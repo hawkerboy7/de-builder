525f1ba4e4b9bd5754ed8406c9658b05379fe64e
dexter77 <dunk_king7@hotmail.com>
2015-05-11 00:33:08 +0200

    default config + npm install fix

--- build.js
+++ build.js
@@ -1,1 +1,16 @@
-require('de-builder')();
+require('de-builder')({
+  src: 'src',
+  build: 'build',
+  client: 'client',
+  server: 'server',
+  less: {
+    file: 'app.less',
+    folder: 'styles'
+  },
+  browserify: {
+    file: 'app.js',
+    folder: 'js'
+  },
+  app: 'app.js',
+  type: 1
+});
