5105f630873f7963cd254c1c401a29e97f15b437
dexter77 <dunk_king7@hotmail.com>
2015-05-11 02:18:06 +0200

    post-install default options support and won't override previous build.js file if it exists already

--- install.js
+++ install.js
@@ -6,11 +6,10 @@
 var fs = require('fs');
 var log = require('de-logger');
 var path = require('path');
-
-// ### TO DO: improve this file so it displays all default options too ###
+var config = require('./build/server/manager/config');
 
 // File content
-var file = "require('de-builder')();";
+var file = "require('de-builder')("+JSON.stringify(config, null, '\t');+");";
 
 // File name
 var name = "build.js";
@@ -18,9 +17,15 @@ var name = "build.js";
 // File location
 var location = path.resolve('../../'+name);
 
-// Create build.js file
-fs.writeFile(location, file, function (err) {
-	if (!err) return;
-	log.warn('LDE - Project', 'Couldn\'t create build.js file');
-	log.error('LDE - Project', err);
+fs.exists(location, function(exists) {
+
+	// Only create file if it doesn't exists yet
+	if (exists) { return log.info('LDE - Project', 'build.js file already exists'); }
+
+	// Create build.js file
+	fs.writeFile(location, file, function (err) {
+		if (!err) return;
+		log.warn('LDE - Project', 'Couldn\'t create build.js file');
+		log.error('LDE - Project', err);
+	});
 });
