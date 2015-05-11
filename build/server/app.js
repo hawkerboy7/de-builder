7a311cc4b801e918fbc9ca8314f5e467b10fda81
dexter77 <dunk_king7@hotmail.com>
2015-05-11 03:20:42 +0200

    Change process name

--- build/server/app.js
+++ build/server/app.js
@@ -2,7 +2,7 @@ var Manager;
 
 Manager = require('./manager');
 
-process.title = "de-builder a Live Development Environment";
+process.title = "de-builder";
 
 module.exports = function(options) {
   return new Manager(options, process.argv[2] === '--start');
