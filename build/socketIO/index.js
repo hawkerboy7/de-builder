bcef8b349efc8636d1df43504ff149470727071a
Dexter Esajas <dunk_king7@hotmail.com>
2016-03-09 12:14:19 +0100

    wrng change of the ports

--- build/socketIO/index.js
+++ build/socketIO/index.js
@@ -16,8 +16,8 @@
     }
 
     SocketIO.prototype.load = function() {
-      this.server.io = io().listen(this.server.config.port);
-      log.info('LDE - Socket.io', this.server.config.title + " events at " + this.server.config.port);
+      this.server.io = io().listen(this.server.config.io);
+      log.info('LDE - Socket.io', this.server.config.title + " events at " + this.server.config.io);
       return this.listeners();
     };
 
